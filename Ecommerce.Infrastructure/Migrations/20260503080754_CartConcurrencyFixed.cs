cf9085f067d1f6a5fe70885cdff46913b40aef27
Idris Abdulkareem <hidytech@gmail.com>
2026-05-03 09:09:56 +0100

    FixCartCocurrencyMigrationUpdated

---
+++ Ecommerce.Infrastructure/Migrations/20260503080754_CartConcurrencyFixed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CartConcurrencyFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CartItem_CartId",
+                table: "CartItem");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItem_CartId",
+                table: "CartItem",
+                column: "CartId");
+        }
+    }
+}
